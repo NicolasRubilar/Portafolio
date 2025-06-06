dcab4edb11b94e9e11356228513c7baccd39c940
Nicolas H. Rubilar <nico.hernandezr@duocuc.cl>
2025-06-05 20:57:31 -0400

    commit_js

--- Portfolio.js/portfolio.js
+++ Portfolio.js/portfolio.js
@@ -1,5 +1,13 @@
 //Modito oscurito
-const toggle = document.getElementById('darkModeToggle');
-toggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode', toggle.checked);
-});
+const toggle =document.getElementById('darkModeToggle');
+
+if (localStorage.getItem('dark-mode')==='true'){
+    document.body.classList.add('dark-mode');
+    toggle.checked=true;
+}
+
+toggle.addEventListener('change',()=>{
+    const isDark =toggle.checked;
+    document.body.classList.toggle('dark-mode',isDark);
+    localStorage.setItem('dark-mode',isDark)
+})
